fc6ec49f1660331ba701aceed2b1443e08d7b652
Check Deng <checkdeng0903@gmail.com>
2021-01-30 13:40:33 +0800

    Replace glob.glob as Path.glob

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-import glob
+from pathlib import Path
 
 libname = "torch_batch_svd"
-ext_src = glob.glob(os.path.join(libname, 'csrc/*.cpp'))
+
+root_dir = Path(libname)
+include_dir = root_dir / 'include'
+ext_src = [str(x.absolute()) for x in root_dir.glob('csrc/*.cpp')]
 print(ext_src)
 
-setup(name=libname,
-      packages=find_packages(exclude=('tests', 'build', 'csrc', 'include', 'torch_batch_svd.egg-info')),
-      ext_modules=[CUDAExtension(
-          libname + '._c',
-          sources=ext_src,
-          libraries=["cusolver", "cublas"],
-          extra_compile_args={'cxx': ['-O2', '-I{}'.format('{}/include'.format(libname))],
-                              'nvcc': ['-O2']}
-      )],
-      cmdclass={'build_ext': BuildExtension.with_options(use_ninja=False)}
-      )
+cuda_extension = CUDAExtension(
+    libname + '._c',
+    sources=ext_src,
+    libraries=["cusolver", "cublas"],
+    extra_compile_args={'cxx': ['-O2', '-I{}'.format(str(include_dir))],
+                        'nvcc': ['-O2']}
+)
+
+setup(
+    name=libname,
+    packages=find_packages(exclude=('tests', 'build', 'csrc',
+                                    'include', 'torch_batch_svd.egg-info')),
+    ext_modules=[cuda_extension],
+    cmdclass={'build_ext': BuildExtension.with_options(use_ninja=False)}
+)
